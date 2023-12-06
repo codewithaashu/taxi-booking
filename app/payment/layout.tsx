7640c9d85146823c60abb19f40a49785cdaed8fb
Ashish Ranjan <88059924+codewithaashu@users.noreply.github.com>
2023-12-07 01:10:11 +0530

    issue resolved 2

--- app/payment/layout.tsx
+++ app/payment/layout.tsx
@@ -3,11 +3,10 @@ import CheckoutForm from "@/components/CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 const Layout = () => {
-  const stripePromise: any = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-  );
+  const arg: any = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+  const stripePromise: any = loadStripe(arg);
 
-  const options = {
+  const options: any = {
     mode: "payment",
     amount: 14000,
     currency: "inr",
